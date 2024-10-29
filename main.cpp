c613e9da5f0a5f80ba16c30f42688ee22b4d6c35
Dking97 <dak.king97@gmail.com>
2024-10-28 21:08:25 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#ifndef GATES_H
+#define GATES_H
+// Logic gate functions
+
+inline bool NOT(bool in) {
+    return !in; // NOT operation
+}
+
+inline bool AND(bool in1, bool in2) {
+    return in1 && in2; // AND operation
+}
+
+inline bool OR(bool in1, bool in2) {
+    return in1 || in2; // OR operation
+}
+
+inline bool XOR(bool in1, bool in2) { //Dakota
+    return in1 ^ in2; // XOR operation
+}
+
+#endif // GATES_H
