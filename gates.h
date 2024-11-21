9057df59c32a287cecc3d4bbdce97f4069bd2301
cyong-csuc <cyong.csuc@gmail.com>
2024-11-21 10:26:47 -0800

    Update gates.h
    AND4 and OR7

--- gates.h
+++ gates.h
@@ -19,4 +19,12 @@ inline bool XOR(bool in1, bool in2) { //Dakota
     return in1 ^ in2; // XOR operation
 }
 
+inline bool AND4(bool in1, bool in2, bool in3, bool in4) {
+    return in1 && in2 && in3 && in4; // AND operation
+}
+
+inline bool OR7(bool in1, bool in2, bool in3, bool in4, bool in5, bool in6, bool in7) {
+    return in1 || in2 || in3 || in4 || in5 || in6 || in7; // OR operation
+}
+
 #endif // GATES_H
